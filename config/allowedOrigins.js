aeecbfccd707e2ab67460e47b2c805bc7112fbd1
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2024-10-13 16:08:26 +0100

    allowed origin added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://www.amirrealtor.com",
   "https://amirrealtor.com",
+  "https://amirrealtor-react-fe.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
